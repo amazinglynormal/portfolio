aa6a26181b9cafbd56497c0bdca0ab568e9f2c32
Stephen <stephenbrady365@gmail.com>
2021-04-13 16:52:43 +0100

    Add responsive navbar and hero section outline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./main.css";
+
+const closeMenuBtn = document.getElementById("close-menu-btn");
+const openMenuBtn = document.getElementById("open-menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+const downloadCVBtns = document.getElementsByClassName("download-cv");
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("hidden");
+}
+
+openMenuBtn.addEventListener("click", toggleMobileMenu);
+closeMenuBtn.addEventListener("click", toggleMobileMenu);
+
+for (const btn of downloadCVBtns) {
+  btn.addEventListener("click", () => {
+    console.log("Download CV");
+  });
+}
