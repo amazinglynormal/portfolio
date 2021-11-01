a55e4a233fc2bb2d29053021f06285ace8399f5b
Stephen <stephenbrady365@gmail.com>
2021-11-01 20:28:11 +0000

    Add responsive navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./main.css";
+
+const openMenuButton = document.getElementById("open-menu-btn");
+const closeMenuButton = document.getElementById("close-menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+openMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.remove("hidden");
+});
+
+closeMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
+});
