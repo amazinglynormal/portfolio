0f8e48bc80ca8bdaeccca12f5d76ef603b004d1a
Stephen <stephenbrady365@gmail.com>
2021-11-03 15:15:07 +0000

    Extend default color scheme and activate dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          base: "#0B0C10",
+          light: "#1F2833",
+        },
+        blue: {
+          electric: "#66FCF1",
+          "darker-electric": "#45A29E",
+        },
+        gray: {
+          taupe: "#8E8D8A",
+          dm: "#C5C6C7",
+        },
+        eggshell: {
+          light: "#EAE7DC",
+          dark: "D8C3A5",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
