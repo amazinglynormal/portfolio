da80c15359c1bcd8ae748ed82b7e1f90e36fd105
Stephen Brady <stephenbrady365@gmail.com>
2021-11-04 20:11:10 +0000

    Add final custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,17 @@
 module.exports = {
   purge: [],
-  darkMode: "media", // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: {
         "abstract-pattern": "url('/assets/abstract-timekeeper.svg')",
       },
       colors: {
-        black: {
-          base: "#0B0C10",
-          light: "#1F2833",
-        },
-        blue: {
-          electric: "#66FCF1",
-          "darker-electric": "#45A29E",
-        },
-        gray: {
-          taupe: "#8E8D8A",
-          dm: "#C5C6C7",
-        },
-        eggshell: {
-          light: "#EAE7DC",
-          dark: "D8C3A5",
-        },
+        'rich-black': "#0B0C10",
+        'bunker': "#1F2833",
+        'electric-blue': "#66FCF1",
+        'electric-blue-dark': "#45A29E",
+        'light-gray': "#C5C6C7",
       },
     },
   },
