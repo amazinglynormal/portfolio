fbc741f20eb521ae864a88050c921fa0a95942b2
Stephen <stephenbrady365@gmail.com>
2022-05-24 19:43:45 +0100

    Update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/index.html"],
   theme: {
     extend: {
       backgroundImage: {
         "abstract-pattern": "url('/assets/abstract-timekeeper.svg')",
       },
       colors: {
-        'rich-black': "#0B0C10",
-        'bunker': "#1F2833",
-        'electric-blue': "#66FCF1",
-        'electric-blue-dark': "#45A29E",
-        'light-gray': "#C5C6C7",
+        "rich-black": "#0B0C10",
+        bunker: "#1F2833",
+        "electric-blue": "#66FCF1",
+        "electric-blue-dark": "#45A29E",
+        "light-gray": "#C5C6C7",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
