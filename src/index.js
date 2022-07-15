c81ab28afe5dc8e2325394dce20e8cd8c5d04571
Stephen <stephenbrady365@gmail.com>
2022-07-15 21:48:08 +0100

    Add hover event for project card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("linked");
+const projectLinkTexts = document.querySelectorAll(".project-link-text");
+const projectArticles = document.querySelectorAll(".project-link");
+
+for (i = 0; i < projectArticles.length; i++) {
+  const element = projectLinkTexts[i];
+  projectArticles[i].addEventListener("mouseenter", () => {
+    element.classList.add("hover");
+  });
+  projectArticles[i].addEventListener("mouseleave", () => {
+    element.classList.remove("hover");
+  });
+}
