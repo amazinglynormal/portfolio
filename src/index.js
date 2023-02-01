24d5011768e6853ff13429333ca8ce69165cb449
Stephen <stephenbrady365@gmail.com>
2023-02-01 08:53:49 +0000

    Fix: Remove unused event listener

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-const projectLinkTexts = document.querySelectorAll(".project-link-text");
-const projectArticles = document.querySelectorAll(".project-link");
-
-for (i = 0; i < projectArticles.length; i++) {
-  const element = projectLinkTexts[i];
-  projectArticles[i].addEventListener("pointerenter", () => {
-    element.classList.add("hover");
-  });
-  projectArticles[i].addEventListener("pointerleave", () => {
-    element.classList.remove("hover");
-  });
-}
-
 const mobileNavMenu = document.querySelector(".mobile-nav");
 
 const toggleMobileNavMenu = () => {
