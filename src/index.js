1aa7ee51ee75f834f798a51438b5b00889ea0f29
Stephen <stephenbrady365@gmail.com>
2023-02-01 16:08:55 +0000

    Func: Add copy to clipboard feature

--- src/index.js
+++ src/index.js
@@ -18,3 +18,24 @@ const mobileNavItems = document.querySelectorAll(
 mobileNavItems.forEach((item) => {
   item.addEventListener("click", toggleMobileNavMenu);
 });
+
+const emailText = document.querySelector(".email-text");
+
+const copyToClipboardBtn = document.querySelector(".clipboard-btn");
+const tooltip = document.querySelector(".tooltip");
+
+const toggleTooltip = () => {
+  copyToClipboardBtn.classList.toggle("clipped");
+  tooltip.classList.toggle("show");
+  tooltip.classList.toggle("hide");
+};
+
+copyToClipboardBtn.addEventListener("click", async () => {
+  try {
+    await navigator.clipboard.writeText(emailText.textContent);
+    toggleTooltip();
+    setTimeout(toggleTooltip, 2000);
+  } catch (error) {
+    console.error("Failed to copy to clipboard", error);
+  }
+});
